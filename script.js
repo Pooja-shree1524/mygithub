791ca2dcfc836f672a95a62d7c9d65d1b6490b4c
Pooja-shree1524 <poojashree.s2024cse@sece.ac.in>
2025-01-21 12:11:20 +0530

    Add files via upload
    This is the js code of the portfolio

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+  
+    const links = document.querySelectorAll("nav ul li a");
+
+    for (const link of links) {
+        link.addEventListener("click", smoothScroll);
+    }
+
+    function smoothScroll(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute("href").substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: "smooth"
+        });
+    }
+
+    
+    document.getElementById("contact-form").addEventListener("submit", function(e) {
+        e.preventDefault();
+        alert("Message sent!");
+        
+    });
+});
